13989987f3432b5c88473bf6b1e37e88cb0eeb8c
ponkinalisa <ponkinalisa77@gmail.com>
2025-02-24 21:09:30 +0700

    задание 6, первое задание в домашней работе

--- mars.py
+++ mars.py
@@ -199,6 +199,37 @@ def astronaut_selection():
         print(request.form['sex'])
         return "Форма отправлена"
 
+@app.route('/choice/<planet_name>')
+def my_suggestion(planet_name):
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Варианты выбора</title>
+                  </head>
+                  <body>
+                    <h1>Мое предложение: {planet_name}</h1>
+                    <h3>Эта планета близка к Земле;</h3>
+                    <div class="p-3 mb-2 bg-success text-white" role="alert">
+                      На ней много необходимых ресурсов;
+                    </div>
+                    <div class="p-3 mb-2 bg-secondary text-white" role="alert">
+                      На ней есть вода и атмосфера;
+                    </div>
+                    <div class="p-3 mb-2 bg-info text-white" role="alert">
+                      На ней есть небольшое магнитное поле;
+                    </div>
+                    <div class="p-3 mb-2 bg-warning text-white" role="alert">
+                      Наконец, она просто красива!
+                    </div>
+                  </body>
+                </html>'''
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
